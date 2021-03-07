d1e079c86e2c09da8e090983919f2bd92c46d3d5
Gusts Kaksis <gusts.kaksis@sonarworks.com>
2021-03-07 10:11:35 +0200

    Add iOS stubs

---
+++ ios/MyCrossPlatformDelegate.mm
@@ -0,0 +1,96 @@
+//
+// Created by Gusts Kaksis on 3/6/21.
+//
+
+#include "MyCrossPlatformClass2.hpp"
+#include "Utilities/JniHelpers.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
+namespace
+{
+
+JNIEnv* jniEnv;
+void cppMethodCall(JNIEnv* jniEnv, jobject thisRef)
+{
+    // Java has called cppmethod
+    auto* nativeObject = getNativePtr<MyCrossPlatformClass2>(jniEnv, thisRef);
+    nativeObject->cppMethod();
+}
+// Method map for MyCrossPlatformClass2 in Java
+const JNINativeMethod methodsArray[] =
+{
+    { "cppMethod", "()V", reinterpret_cast<void*>(&cppMethodCall) }
+};
+
+}
+
+MyCrossPlatformClass2::MyCrossPlatformClass2()
+{
+    auto javaClass = jniEnv->FindClass("com/example/cppcrossplatform/MyCrossPlatformClass2");
+    if (!javaClass)
+    {
+        throw std::runtime_error("Failed to find MyCrossPlatformClass2 in JNI environment");
+    }
+    auto constructorId = jniEnv->GetMethodID(javaClass, "<init>", "(J)V");
+    if (!constructorId)
+    {
+        throw std::runtime_error("Failed to find MyCrossPlatformClass2 constructor");
+    }
+    // Create object and pass this pointer as it's first argument for storage
+    auto localObject = jniEnv->NewObject(javaClass, constructorId, reinterpret_cast<jlong>(this));
+    if (!localObject)
+    {
+        throw std::runtime_error("Failed to create MyCrossPlatformClass2");
+    }
+    // Last but not least we need to create global reference so we can keep owning the Java companion class
+    jniObject = jniEnv->NewGlobalRef(localObject);
+}
+
+MyCrossPlatformClass2::~MyCrossPlatformClass2()
+{
+    // Release the global reference
+    jniEnv->DeleteGlobalRef(jniObject);
+}
+
+void MyCrossPlatformClass2::platformNativeMethod() const
+{
+    auto javaClass = jniEnv->FindClass("com/example/cppcrossplatform/MyCrossPlatformClass2");
+    if (!javaClass)
+    {
+        throw std::runtime_error("Failed to find MyCrossPlatformClass2 in JNI environment");
+    }
+    auto methodId = jniEnv->GetMethodID(javaClass, "nativeMethod", "()V");
+    if (!methodId)
+    {
+        throw std::runtime_error("Failed to find nativeMethod in MyCrossPlatformClass2");
+    }
+    jniEnv->CallVoidMethod(jniObject, methodId);
+}
+
+void MyCrossPlatformClass2::cppMethod() const noexcept
+{
+    std::cout << "C++ method called\n";
+}
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* /* reserved */)
+{
+    // Capture current environment for later use
+    if (vm->GetEnv(reinterpret_cast<void**>(&jniEnv), JNI_VERSION_1_6) != JNI_OK)
+    {
+        return JNI_ERR;
+    }
+    // Find the class definition in the current environment
+    auto javaClass = jniEnv->FindClass("com/example/cppcrossplatform/MyCrossPlatformClass2");
+    if (!javaClass)
+    {
+        return JNI_ERR;
+    }
+    // Register method map for the class
+    if (jniEnv->RegisterNatives(javaClass, methodsArray, sizeof(methodsArray) / sizeof(methodsArray[0])) < 0)
+    {
+        return JNI_ERR;
+    }
+    return JNI_VERSION_1_6;
+}
